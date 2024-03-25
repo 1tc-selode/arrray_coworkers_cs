16b30cdd5566d99b5a21aba8b3dc82987e37859f
Odett2007 <131852028+Odett2007@users.noreply.github.com>
2024-03-25 11:19:24 +0100

    Add project files.

---
+++ arrray_coworkers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrray_coworkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Odett: Tömb létrehozása
+
+            //Patrik: Tömb adatainak összeadása
+
+            //Máté: Eredmények kiírása
+        }
+    }
+}
